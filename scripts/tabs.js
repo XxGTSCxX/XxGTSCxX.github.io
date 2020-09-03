3c7fe671f3a17587c7e6850715411013387b102f
Gabrielle Tan <g.tan@digipen.edu>
2020-09-03 21:09:58 +0800

    [Reorganisation]

---
+++ scripts/tabs.js
@@ -0,0 +1,21 @@
+function OpenTab(event, tab_id)
+{
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-content");
+
+    for (i = 0; i < tabcontent.length; i++)
+    {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab-links");
+
+    for (i = 0; i < tablinks.length; i++)
+    {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tab_id).style.display  = "block";
+    event.currentTarget.className                 += " active";
+}
