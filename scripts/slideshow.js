48de8f23faa9b12e6ab0217a97ab98451631a3c5
Gabrielle Tan <g.tan@digipen.edu>
2020-11-17 19:29:32 +0800

    [Slideshow - Updated]
    - Updated script for slideshows to be generalised.

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,45 +1,61 @@
-var current_slide = 0;
+let slide_positions = new Map();
+
+function SlidesInit()
+{
+  var containers = document.querySelectorAll(".slideshow-container");
+  containers.forEach(container =>
+  {
+    ShowSlide(container.id, 0);
+  });
+}
 
 // Next/previous controls
-function NavigateSlides(index_offset)
+function NavigateSlides(slide_set_id, index_offset)
 {
-  ShowSlide(current_slide + index_offset);
+  if (slide_positions.has(slide_set_id))
+  {
+    ShowSlide(slide_set_id, slide_positions.get(slide_set_id) + index_offset);
+  }
 }
 
 // Thumbnail image controls
-function CurrentSlide(new_index)
+function CurrentSlide(slide_set_id, new_index)
 {
-  ShowSlide(new_index);
+  ShowSlide(slide_set_id, new_index);
 }
 
-function ShowSlide(slide_index)
+function ShowSlide(slide_set_id, slide_index)
 {
-  var slides = document.querySelectorAll(".slide");
-  var dots   = document.querySelectorAll(".dot"  );
+  var container = document.getElementById(slide_set_id);
 
-  if (slides.length && slides.length == dots.length)
+  if (container.classList.contains("slideshow-container"))
   {
-    current_slide = slide_index;
+    var slides = container.querySelectorAll(".slide");
+    var dots   = container.querySelectorAll(".dot"  );
 
-    while (current_slide < 0)
+    if (slides.length && slides.length == dots.length)
     {
-      current_slide += slides.length;
+      while (slide_index < 0)
+      {
+        slide_index += slides.length;
+      }
+
+      slide_index %= slides.length;
+
+      slides.forEach(slide =>
+      {
+        slide.style.display = "none";
+      });
+
+      dots.forEach(dot =>
+      {
+        dot.className = dot.className.replace(" active", "");
+      });
+
+      slides[slide_index].style.display  = "block";
+      dots  [slide_index].className     += " active";
+      slide_positions.set(slide_set_id, slide_index);
+      window.onresize();
     }
-
-    current_slide %= slides.length;
-
-    slides.forEach(slide =>
-    {
-      slide.style.display = "none";
-    });
-
-    dots.forEach(dot =>
-    {
-      dot.className = dot.className.replace(" active", "");
-    });
-
-    slides[current_slide].style.display  = "block";
-    dots  [current_slide].className     += " active";
-    window.onresize();
   }
 }
